36c51c82c316c7c4dc62e2142d2f02d3638b6659
andrea-everett <andrea.dawn.everett@gmail.com>
2023-03-27 17:57:44 -0400

    fix styling and nav bar

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const dropDownNavButton = document.querySelector('.hamburger');
-const topNavRight = document.querySelector('.topnav-right');
+// const dropDownNavButton = document.querySelector('.hamburger');
+// const topNavRight = document.querySelector('.topnav-right');
 
-dropDownNavButton.addEventListener("click", () => {
-    console.log("click")
-    topNavRight.classList.remove('topnav-right')
-    topNavRight.classList.add('active-topnav')
+// dropDownNavButton.addEventListener("click", () => {
+//   console.log("click")
+//   topNavRight.classList.remove('topnav-right')
+//   topNavRight.classList.add('active-topnav')
+// })
+const hamburger = document.querySelector(".hamburger");
+const topNavRight = document.querySelector(".topnav-right");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("topnav-right");
+  topNavRight.classList.toggle("active-topnav");
 })
+
+
+// const dropDownNavButton = document.querySelector('.hamburger');
+// const topNavRight = document.querySelector('.topnav-right');
+
+// function toggleMenu() {
+//   if (topnav - right.classList.contains("topnav-right")) {
+//     menu.classList.remove("topnav-right");
+//     closeIcon.style.display = "none";
+//     menuIcon.style.display = "block";
+//   } else {
+//     menu.classList.add("active-topnav");
+//     closeIcon.style.display = "block";
+//     menuIcon.style.display = "none";
+//   }
+// }
+
+// dropDownNavButton.addEventListener("click", toggleMenu);
